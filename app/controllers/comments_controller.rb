47d6f32aeb4569d00a01d5f20da509ded34b0b94
syberpunkq <syberpunk@gmail.com>
2014-12-09 23:02:25 +0000

    Second try

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.create(comment_params)
+    redirect_to task_path(@task)
+  end
+  
+  def destroy
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.find(params[:id])
+    @comment.destroy
+    redirect_to task_path(@task)
+  end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,64 @@
+class TasksController < ApplicationController
+  
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
+  def index
+    @tasks = Task.all    
+    
+  end
+  
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+#   def new
+#   end
+  
+#   def create
+#     @task = Task.new(params.require(:task).permit(:title, :text))
+#     @task.save
+#     redirect_to @task
+#     #render plain: params[:task].inspect
+#   end
+def new
+  @task = Task.new
+end
+
+def edit
+  @task = Task.find(params[:id])    
+end
+  
+def create
+  @task = Task.new(task_params)
+
+  if @task.save
+    redirect_to @task
+  else
+    render 'new'
+  end
+end
+
+def update
+  @task = Task.find(params[:id])
+ 
+  if @task.update(task_params)
+    redirect_to @task
+  else
+    render 'edit'
+  end
+end  
+  
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+ 
+  redirect_to tasks_path
+end
+  
+private
+  def task_params
+    params.require(:task).permit(:title, :text)
+  end
+  
+end
