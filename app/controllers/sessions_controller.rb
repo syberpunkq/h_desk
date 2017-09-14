730759436afa2cc14923ff9bbe5b16dcf5acfe40
syberpunkq <syberpunkq@gmail.com>
2017-09-14 23:41:44 +0300

    hdesk01 Добавить авторизацию в тестовом режиме

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:email])
-  	if user && user.authenticate(params[:password])
+    key = Rails.env.development? ? :login : :email
+    hash = {key => params[key]}
+
+    user = User.find_by(hash)
+
+  	if user && (Rails.env.development? || user.authenticate(params[:password]))
   		session[:user_id] = user.id
   		redirect_to root_url, notice: "Logged in"
   	else
   		flash[:alert] = "Email or password is invalid"
   		render "new"
   	end
+
   end
   def destroy
   	session[:user_id] = nil
