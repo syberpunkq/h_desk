c2fe900e96b354d9e2c987b9ba768bdab639fd9e
syberpunkq <syberpunkq@gmail.com>
2017-10-08 15:12:29 +0300

    Добавил категории

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513120607) do
+ActiveRecord::Schema.define(version: 20171008120723) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
